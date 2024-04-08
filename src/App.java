6cb58ab8fc872104b3150a66cfd7365aa8633900
Junnel Reboquio <jreboqui@gmail.com>
2024-04-07 23:32:54 -0700

    Clean up commented out code

--- src/App.java
+++ src/App.java
@@ -1,18 +1,7 @@
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 import pageObjects.GameBoardPage;
 
-import java.time.Duration;
-
-
 public class App extends BaseApp {
-    static WebElement inputBox = null;
-   
-    static int fakeGold = -1;
-    static int n = 9; //number of gold bars to check
 
     public static void main(String[] args) throws Exception {
         
@@ -24,142 +13,8 @@ public class App extends BaseApp {
        gameBoardPage.start(); 
        
        System.out.println("End of program");
-        // for (int i = 0; i < arr.length; i++) {
-        //     //Get the input box
-        //     if (i < mid) {
-        //         inputBox = driver.findElement(By.id("left_" + arr[i]));
-        //         inputBox.sendKeys(String.valueOf(i));
-        //     } else if (i > mid){
-        //         inputBox = driver.findElement(By.id("right_" + arr[i]));
-        //         inputBox.sendKeys(String.valueOf(i));
-        //     } 
-        // }        
-        // WebElement weighButton = driver.findElement(By.id("weigh"));
-        // //click on the weigh button
-        // weighButton.click();
-        // Thread.sleep(5000);
-
-        // WebElement resultElement = driver.findElement(By.xpath("//div[@class='result']/button[@id='reset']"));
-        // String result = resultElement.getAttribute("innerText");
-        // if (result.equals("=")) {
-        //     WebElement box4 = driver.findElement(By.id("coin_4"));
-        // }
-
-        //  //get the result and check which weighs less
-    
-        // System.out.println("The result is: " + result);
-
-        //click reset
-        // //div[@class='game']//div//button[@id="reset"]
-        // boolean found = false;
-        // while (!found) {
-        //     found = recursiveSplitAndWeigh();
-        // }
-      //  recursiveSplitAndWeigh(arr, low, high, mid);
-
-        
-        
-        // //Get the weighings
-        // List<WebElement> weighings = driver.findElements(By.xpath("//div[@class='game-info']//li"));
-        // //Print the weighings
-        // for (WebElement weighing : weighings) {
-        //     System.out.println(weighing.getText());
-        // }
     }
-
-    public static void recursiveSplitAndWeigh(int[] arr, int low, int high, int mid) throws InterruptedException{
-
-        System.out.println("In recursive start, n is: " + n);
-        if (n <= 1) return;
-
-        WebElement clearWeighBowls = driver.findElement(By.xpath("//button[contains(text(),'Reset')]"));
-        clearWeighBowls.click();
-        Thread.sleep(5000);
-
-         for (int i = low; i <= high; i++) {
-            //Get the input box
-            if (n % 2 != 0 || n < 2) {
-                if (i < mid) {
-                            inputBox = driver.findElement(By.id("left_" + arr[i]));
-                            inputBox.sendKeys(String.valueOf(i));
-                } else if (i > mid){
-                    inputBox = driver.findElement(By.id("right_" + arr[i]));
-                    inputBox.sendKeys(String.valueOf(i));
-                } 
-            } else {
-                if (i <= mid) {
-                    inputBox = driver.findElement(By.id("left_" + arr[i]));
-                    inputBox.sendKeys(String.valueOf(i));
-                } else {
-                    inputBox = driver.findElement(By.id("right_" + arr[i]));
-                    inputBox.sendKeys(String.valueOf(i));
-                } 
-            }
-           
-        }
-        n = n/2;        
-
-        WebElement weighButton = driver.findElement(By.id("weigh"));
-        //click on the weigh button
-        weighButton.click();
-        Thread.sleep(5000);
-        //get the result and check which weighs less
-      //  WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(10));
-
-        //Thread.sleep(5000);
-        System.out.println("Checking for result");
-        WebElement resultElement = driver.findElement(By.xpath("//div[@class='result']/button[@id='reset']"));
-        
-        //Check the result
-        String result = resultElement.getAttribute("innerText");
-        System.out.println("The result is: " + result);
-     
-        if (result.equals("=")) {
-            WebElement box4 = driver.findElement(By.id("coin_4"));
-            box4.click();
-            return;
-        } else if (result.equals("<")) {
-            System.out.println("Less than scenario");
-            //Call recursive function with first half
-           // enterValues(arr, low, mid-1, (low+(mid-1))/2);
-           System.out.println("New low is: " + low);
-           System.out.println("New high is: " + (mid-1));
-           System.out.println("New mid is: " + (low+(mid-1))/2);
-            recursiveSplitAndWeigh(arr, low, mid-1, (low+(mid-1))/2);
-        } else {
-            //Call recursive function with second half
-            System.out.println("Greater than");
-           // enterValues(arr, mid+1, high, ((mid+1)+high)/2);
-           System.out.println("New low is: " + (mid+1));
-           System.out.println("New mid is: " + ((mid+1)+high)/2);
-           System.out.println("New high is: " + high);
-            recursiveSplitAndWeigh(arr, mid+1, high, ((mid+1)+high)/2);
-        }
     
-    }
-
-    // public static void enterValues(int[] arr, int count, int firstIndex, int lastIndex){
-    //     System.out.println("EnterValues start");
-
-    //     WebElement clearWeighBowls = driver.findElement(By.xpath("//button[contains(text(),'Reset')]"));
-    //     clearWeighBowls.click();
-        
-    //     System.out.println("Entering values from " + firstIndex + " ending on " + lastIndex + " and n is " + n);
-    //     for (int i = firstIndex; i <= lastIndex; i++) {
-    //         //Get the input box
-    //         if (i <= (firstIndex+lastIndex)/2) {
-    //             inputBox = driver.findElement(By.id("left_" + i));
-    //             inputBox.sendKeys(String.valueOf(i));
-    //         } else {
-    //             inputBox = driver.findElement(By.id("right_" + i));
-    //             inputBox.sendKeys(String.valueOf(i));
-    //         }
-    //         WebElement weighButton = driver.findElement(By.id("weigh"));
-    //         weighButton.click();
-    //     }
-    //     System.out.println("Done entering values");
-    //     n = n/2;
-    // }
 }
 
 
--- src/pageObjects/GameBoardPage.java
+++ src/pageObjects/GameBoardPage.java
@@ -1,6 +1,5 @@
 package pageObjects;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -11,17 +10,9 @@ import pageComponents.WeighingScaleComponent;
 import pageComponents.WeighingsComponent;
 
 public class GameBoardPage {
-    @FindBy(xpath = "//h1")
-    WebElement Header;
-
-    @FindBy(xpath = "//button[contains(text(),'Reset')]")
-    WebElement clearWeighBowls;
 
     @FindBy(id = "weigh")
     WebElement weighButton;
-
-    @FindBy(xpath = "//div[@class='result']/button[@id='reset']")
-    WebElement resultElement;
 
     WebDriver driver;
 
@@ -43,17 +34,11 @@ public class GameBoardPage {
 
     }
 
-
-
     public void start(){
-        //Get how many gold bars we are going to play with based on the board
-        coinsComponent.setCoins();
-
-        setInitialLowMidHigh(coinsComponent.getCoinsArray());
+        init();
 
         try {
-            answer = recursiveSplitAndWeigh(coinsComponent.getCoinsArray(),
-             initialLow, initialHigh, coinsComponent.getNumOfCoins());
+            answer = recursiveSplitAndWeigh(initialLow, initialHigh, coinsComponent.getNumOfCoins());
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
@@ -66,19 +51,25 @@ public class GameBoardPage {
 
     }
 
+    private void init() {
+        setGameCoins();
+        setInitialLowMidHigh();
+    }
+
+    private void setGameCoins() {
+        coinsComponent.setCoins();
+    }
+
     private void verifyWinnerMessage() {
         //get the message from the alert
         System.out.println(driver.switchTo().alert().getText());
 
     }
 
-    //It takes the last item on the weighing list and determines which coin box to select
+    //The answer is returned by the recursive weighing at the end
     private void selectCorrectAnswer() {
-      
-        coinsComponent.clickOnCoin(answer);
-
+      coinsComponent.clickOnCoin(answer);
     }
-
 
     private void outputListOfWeighings() {
        for (WebElement weighing : weighingsComponent.getWeighingList()){
@@ -86,94 +77,21 @@ public class GameBoardPage {
        }
     }
 
-
-    // private int recursiveSplitAndWeigh(int[] arr, int low, int mid, int high, int numOfCoins) throws InterruptedException{
-    //     System.out.println("In recursive start, n is: " + numOfCoins);
-    //     if (numOfCoins <= 1) return low;
-
-       
-    //     clearWeighBowls.click();
-    //     Thread.sleep(5000);
-
-    //      for (int i = low; i <= high; i++) {
-    //         //Get the input box
-    //         if (numOfCoins % 2 != 0 || numOfCoins < 2) {
-    //             if (i < mid) {
-    //                 inputBox = driver.findElement(By.id("left_" + arr[i]));
-    //                 inputBox.sendKeys(String.valueOf(i));
-    //             } else if (i > mid){
-    //                 inputBox = driver.findElement(By.id("right_" + arr[i]));
-    //                 inputBox.sendKeys(String.valueOf(i));
-    //             } 
-    //         } else {
-    //             if (i <= mid) {
-    //                 inputBox = driver.findElement(By.id("left_" + arr[i]));
-    //                 inputBox.sendKeys(String.valueOf(i));
-    //             } else {
-    //                 inputBox = driver.findElement(By.id("right_" + arr[i]));
-    //                 inputBox.sendKeys(String.valueOf(i));
-    //             } 
-    //         }
-           
-    //     }
-    //     numOfCoins = numOfCoins/2;        
-
-       
-    //     //click on the weigh button
-    //     weighButton.click();
-    //     //get the result and check which weighs less
-    //   //  WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(10));
-
-    //     Thread.sleep(5000);
-    //     System.out.println("Checking for result");
-        
-        
-    //     //Check the result
-    //     String result = resultElement.getAttribute("innerText");
-    //     System.out.println("The result is: " + result);
-     
-    //     if (result.equals("=")) {
-    //         return mid; //Base case: found the fake gold in the middle
-    //         } else if (result.equals("<")) {
-    //         System.out.println("Less than scenario");
-    //         //Call recursive function with first half
-    //        // enterValues(arr, low, mid-1, (low+(mid-1))/2);
-    //        System.out.println("New low is: " + low);
-    //        System.out.println("New mid is: " + (low+((mid-1)-low))/2);
-    //        System.out.println("New high is: " + (mid-1));
-          
-    //          return recursiveSplitAndWeigh(arr, low, (low+(mid-1))/2,  mid-1, numOfCoins);
-    //     } else {
-    //         //Call recursive function with second half
-    //         System.out.println("Greater than");
-    //        // enterValues(arr, mid+1, high, ((mid+1)+high)/2);
-    //        System.out.println("New low is: " + (mid+1));
-    //        System.out.println("New mid is: " + ((mid+1)+high)/2);
-    //        System.out.println("New high is: " + high);
-    //          return recursiveSplitAndWeigh(arr, mid+1, ((mid+1)+high)/2, high, numOfCoins);
-    //     }
-    // }
-
-    private void setInitialLowMidHigh(int[] coinsArray) {
-        initialLow = coinsArray[0];
-        initialHigh = coinsArray[coinsArray.length-1]; 
+    private void setInitialLowMidHigh() {
+        initialLow = 0;
+        initialHigh = coinsComponent.getNumOfCoins()-1; 
     }
 
-    private int recursiveSplitAndWeigh(int[] arr, int low, int high, int numOfCoins) throws InterruptedException{
+    private int recursiveSplitAndWeigh(int low, int high, int numOfCoins) throws InterruptedException{
         System.out.println("In recursive start, n is: " + numOfCoins);
         if (numOfCoins <= 1) return low;
 
         int mid = (numOfCoins == 2) ? low : (low+high)/2;
         high = (numOfCoins == 2) ? mid+1: high;
         
-        clearWeighBowls.click();
-        Thread.sleep(5000);
-
-        weighingScaleComponent.enterValuesAndWeigh(arr, low, high, mid, numOfCoins);
-
-        //This is where the for loop used to be//
-
-        //end of for loop
+        clearWeighBowls();
+        
+        weighingScaleComponent.enterValuesAndWeigh(low, high, mid, numOfCoins);
        
         numOfCoins = numOfCoins/2;        
 
@@ -181,34 +99,25 @@ public class GameBoardPage {
         //click on the weigh button
         weighButton.click();
         //get the result and check which weighs less
-      //  WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(10));
+        String result = checkResult();
 
-        Thread.sleep(5000);
-        System.out.println("Checking for result");
-        
-        
-        //Check the result
-        String result = resultElement.getAttribute("innerText");
-        System.out.println("The result is: " + result);
-     
         if (result.equals("=")) {
             return mid; //Base case: found the fake gold in the middle
         } else if (result.equals("<")) {
-            System.out.println("Less than scenario");
             //Call recursive function with first half
-           // enterValues(arr, low, mid-1, (low+(mid-1))/2);
-           System.out.println("New low is: " + low);
-           System.out.println("New high is: " + (mid-1));
-          
-            return recursiveSplitAndWeigh(arr, low, mid-1, numOfCoins);
+            return recursiveSplitAndWeigh(low, mid-1, numOfCoins);
         } else {
             //Call recursive function with second half
-            System.out.println("Greater than");
-           // enterValues(arr, mid+1, high, ((mid+1)+high)/2);
-           System.out.println("New low is: " + (mid+1));
-           System.out.println("New high is: " + high);
-             return recursiveSplitAndWeigh(arr, mid+1, high, numOfCoins);
+             return recursiveSplitAndWeigh(mid+1, high, numOfCoins);
         }
+    }
+
+    private void clearWeighBowls() {
+        weighingScaleComponent.clickClearWeighBowlsButton();
+    }
+
+    private String checkResult() {
+        return weighingScaleComponent.getResult();
     }
 
 }
